0aa748ced749d54c41b0d79b2ef1619ae7daaec5
Nejat Can <ncan@hermia.mrel.usc.edu>
2023-07-17 11:27:58 -0700

    changed dual_te_vds_STCR_recon.m

--- dual_te_vds_STCR_recon.m
+++ dual_te_vds_STCR_recon.m
@@ -12,6 +12,7 @@ nfile       = length(all_dat);
 % naverage    = 1;
 narm_frame  = 2;
 para.Recon.narm = narm_frame;   % number of arms per frame
+para.Recon.time_frames = 'all'; % set to 'all' for reconstructructing all frames. specify certain range if you wish, i.e., 1:100
 % nprep       = 5;
 ifsave      = 1;
 file_index = 1;
@@ -37,14 +38,12 @@ echo_idx = view_order > nview;
 echo_idx = echo_idx + 1;
 
 %% size
-nsample         = size(kspace, 2);
+nsample         = size(kspace, 1);
 ncoil           = size(kspace, 3);
 
 %% split kspace
 kspace_echo_1 = kspace(:, echo_idx == 1, :);
-kspace_echo_1 = permute(kspace_echo_1, [2,1,3]);
 kspace_echo_2 = kspace(:, echo_idx == 2, :);
-kspace_echo_2 = permute(kspace_echo_2, [2,1,3]);
 %clear kspace
 
 narm_total      = min(size(kspace_echo_1, 2), size(kspace_echo_2, 2));
@@ -57,45 +56,60 @@ narm_total = nframes * narm_frame;
 kspace_echo_1(:, narm_total + 1 : end, :) = [];
 kspace_echo_2(:, narm_total + 1 : end, :) = [];
 
-nsample_1 = size(kspace_echo_1, 1);
-nsample_2 = size(kspace_echo_2, 1);
+kspace_echo_1  = reshape(kspace_echo_1, [nsample, narm_frame, nframes, ncoil]);
+kspace_echo_2  = reshape(kspace_echo_2, [nsample, narm_frame, nframes, ncoil]);
 
-kspace_echo_1  = reshape(kspace_echo_1, [nsample_1, narm_frame, nframes, ncoil]);
-kspace_echo_2  = reshape(kspace_echo_2, [nsample_2, narm_frame, nframes, ncoil]);
-
-%{
+%% kspace trajectory and view order
 view_order_echo_1 = view_order(echo_idx == 1);
 view_order_echo_2 = view_order(echo_idx == 2);
 view_order_echo_1(narm_total + 1 : end) = [];
 view_order_echo_2(narm_total + 1 : end) = [];
 view_order_echo_1  = reshape(view_order_echo_1, [narm_frame, nframes]);
 view_order_echo_2  = reshape(view_order_echo_2, [narm_frame, nframes]);
-%}
-%% gridding 
+
 kx = kspace_info.kx_GIRF;
 ky = kspace_info.ky_GIRF;
 
+kx_echo_1 = zeros(size(kspace_echo_1,1), size(kspace_echo_1,2), size(kspace_echo_1,3));
+ky_echo_1 = kx_echo_1;
+%kx_echo_1 = zeros(nsample, narm_frame, nframes);
+kx_echo_2 = zeros(size(kspace_echo_2,1), size(kspace_echo_2,2), size(kspace_echo_2,3));
+ky_echo_2 = kx_echo_2;
+%kx_echo_1 = zeros(nsample, narm_frame, nframes);
 
+for i = 1:narm_frame
+    for j = 1:nframes
+        kx_echo_1(:,i,j) = kx(:, view_order_echo_1(i,j));
+        kx_echo_2(:,i,j) = kx(:, view_order_echo_2(i,j) - 10);
+        
+        ky_echo_1(:,i,j) = ky(:, view_order_echo_1(i,j));
+        ky_echo_2(:,i,j) = ky(:, view_order_echo_2(i,j) - 10);
+    end
+end
 
+%% matrix size
 res = [kspace_info.user_ResolutionX, kspace_info.user_ResolutionY];
 fov = [kspace_info.user_FieldOfViewX, kspace_info.user_FieldOfViewY];
 matrix_size_keep    = round(fov ./ res / 2) * 2;
 matrix_size         = round(matrix_size_keep * 2 / 2) * 1.5;
 para.Recon.matrix_size = matrix_size;
 
-
 %% select time frames - echo 1
 if isnumeric(para.Recon.time_frames)
     time_frames = para.Recon.time_frames;
     
-    kx = kx(:, :, time_frames);
-    ky = ky(:, :, time_frames);
-    kSpace = kSpace(:, :, time_frames, :);
+    kx_echo_1 = kx_echo_1(:, :, time_frames);
+    ky_echo_1 = ky_echo_1(:, :, time_frames);
+    kspace_echo_1 = kspace_echo_1(:, :, time_frames, :);
+    
+    kx_echo_2 = kx_echo_2(:, :, time_frames);
+    ky_echo_2 = ky_echo_2(:, :, time_frames);
+    kspace_echo_2 = kspace_echo_2(:, :, time_frames, :);
 end
 
 para.Recon.FOV = 4.8; %units of decimeter for some reason
-Data.N = NUFFT.init(kx * matrix_size(1), ky * matrix_size(2), 1, [6, 6], matrix_size(1), matrix_size(1));
-%Data.N = NUFFT.init(kx*para.Recon.FOV, ky*para.Recon.FOV, 1, [4, 4], para.Recon.matrix_size(1)*para.Recon.FOV, para.Recon.matrix_size(1)*para.Recon.FOV);
+
+Data.N = NUFFT.init(kx_echo_1*para.Recon.FOV, ky_echo_1*para.Recon.FOV, 1, [4, 4], para.Recon.matrix_size(1)*para.Recon.FOV, para.Recon.matrix_size(1)*para.Recon.FOV);
 Data.N.W = kspace_info.DCF(:, 1);
 
 Data.kSpace = kspace_echo_1;
@@ -113,8 +127,7 @@ para.Recon.weight_tTV = scale * para.weight_tTV; % temporal regularization weigh
 para.Recon.weight_sTV = scale * para.weight_sTV; % spatial regularization weight
 
 para.setting.ifplot = 1;        % display image and cost during reconstruction
-para.setting.ifGPU = 0;         % set to 1 when you want to use GPU
-para.Recon.time_frames = 'all'; % set to 'all' for reconstructing all frames
+para.setting.ifGPU = 1;         % set to 1 when you want to use GPU
 
 para.Recon.no_comp = ncoil;     % number of coils
 %para.Recon.FOV = 1.25;         % reconstruction FOV
@@ -135,10 +148,9 @@ im_echo_1 = crop_half_FOV(Image_recon, para.Recon.matrix_size);
 
 %% select time frames - echo 2
 Data.kSpace = kspace_echo_2;
+Data.N = NUFFT.init(kx_echo_1*para.Recon.FOV, ky_echo_1*para.Recon.FOV, 1, [4, 4], para.Recon.matrix_size(1)*para.Recon.FOV, para.Recon.matrix_size(1)*para.Recon.FOV);
 
 Data.first_est = NUFFT.NUFFT_adj(Data.kSpace, Data.N);
-
-Data.first_est = first_est_2;
 
 scale = max(abs(Data.first_est(:)));
 
@@ -154,7 +166,8 @@ im_echo_2 = crop_half_FOV(Image_recon, para.Recon.matrix_size);
 
 %clearvars -except im_echo_1 im_echo_2 kspace_info
 
-%% save 
+%{
+%%save 
 f_mag = imageMRI(sos([im_echo_1(:, :, end, :), im_echo_2(:, :, end, :)]));
 f_phase = imageMRI(angle([im_echo_1(:, :, end, 1), im_echo_2(:, :, end, 1)]));
 if ifsave
@@ -165,3 +178,5 @@ if ifsave
 end
 
 toc
+
+%}

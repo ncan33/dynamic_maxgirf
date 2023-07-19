faed19ca2be8befa03e55d08cb64c19cca80539a
Nejat Can <ncan@hermia.mrel.usc.edu>
2023-07-19 16:20:50 -0700

    bug fixes

--- dual_te_STCR_parameter_sweep.m
+++ dual_te_STCR_parameter_sweep.m
@@ -61,18 +61,18 @@ function sweep = dual_te_STCR_parameter_sweep(narm_frame, tTV_low, tTV_high, ste
         for j = 1:length(sTV_sweep)
             [im_echo_1, im_echo_2, NUFFT_im_echo_1, NUFFT_im_echo_2, kspace_info, para] = dual_te_STCR_wrapper(narm_frame, tTV_sweep(i), initial_sTV_sweep, niter, 0, ifGPU, 0);
             if ifsave
-                save_name = sprintf(['./recon_data/parameter_sweep', num2str(narm_frame), 'arm_', num2str(para.weight_tTV), '_tTV_', num2str(para.weight_sTV),'_sTV_','%s_recon.mat'], dir(path).name(1:end-8));
+                save_name = sprintf(['./recon_data/parameter_sweep/', num2str(narm_frame), 'arm_', num2str(para.weight_tTV), '_tTV_', num2str(para.weight_sTV),'_sTV_','%s_recon.mat'], dir(path).name(1:end-8));
                 save(save_name, 'im_echo_1', 'im_echo_2', 'NUFFT_im_echo_1', 'NUFFT_im_echo_2', 'kspace_info', 'para', '-v7.3');
             end
         end
     end
     
     %% imtile time -- echo 1 only for simplicity
-    clearvars -except tTV_sweep sTV_sweep
+    clearvars -except tTV_sweep sTV_sweep narm_frame path
     
     for i = 1:length(tTV_sweep)
         for j = 1:length(sTV_sweep)
-            load_name = sprintf(['./recon_data/parameter_sweep', num2str(narm_frame), 'arm_', num2str(tTV_sweep(i)), '_tTV_', num2str(sTV_sweep(j)),'_sTV_','%s_recon.mat'], dir(path).name(1:end-8));
+            load_name = sprintf(['./recon_data/parameter_sweep/', num2str(narm_frame), 'arm_', num2str(tTV_sweep(i)), '_tTV_', num2str(sTV_sweep(j)),'_sTV_','%s_recon.mat'], dir(path).name(1:end-8));
             load(load_name, 'im_echo_1');
             
             if i == 1 && j == 1

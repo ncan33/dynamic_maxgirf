118c929c3a432ef15e69b5bca30cabf2fd303790
Nejat Can <ncan@u.rochester.edu>
2023-08-04 19:22:13 -0700

    ready for recon (hopefully?)

--- demo_non_cartesian_recon_human_axial.m
+++ demo_non_cartesian_recon_human_axial.m
@@ -7,8 +7,8 @@
 close all; clear all; clc;
 
 %% Set source directories
-src_directory = '/server/home/nlee/lowfield_maxgirf';
-ismrmrd_directory = '/server/home/nlee/ismrmrd';
+src_directory = '/server/home/ncan/GitHub/lowfield_maxgirf';
+ismrmrd_directory = '/server/home/ncan/ismrmrd';
 
 %% Add source directories to search path
 addpath(genpath(src_directory));
@@ -18,7 +18,7 @@ addpath(genpath(ismrmrd_directory));
 ismrmrd_noise_path = '/scratch/nlee/NHLBI/data/20201102_NV_brain/noise/noise_meas_MID00275_FID03658_se_spiral_1102_ax_s24.h5';
 ismrmrd_data_path = '/scratch/nlee/NHLBI/data/20201102_NV_brain/h5/meas_MID00275_FID03658_se_spiral_1102_ax_s24.h5';
 siemens_dat_path = '/scratch/nlee/NHLBI/data/20201102_NV_brain/meas_MID00275_FID03658_se_spiral_1102_ax_s24.dat';
-B0map_fullpath = '/server/home/nlee/lowfield_maxgirf/B0map_nlinv_min1.0e-06_axial.mat';
+B0map_fullpath = '/server/home/ncan/GitHub/lowfield_maxgirf/B0map_nlinv_min1.0e-06_axial.mat';
 
 %% Set reconstruction options
 % "phase_sign" and "read_sign" can be determined only from Siemens raw data 
@@ -67,7 +67,7 @@ end
 [im_maxgirf_gpu, header_maxgirf, r_dcs_maxgirf, output_maxgirf] = siemens_maxgirf_cg_recon_single_gpu(ismrmrd_noise_path, ismrmrd_data_path, siemens_dat_path, B0map_nlinv, user_opts);
 save(sprintf('%s_maxgirf_single_gpu_supp%d_iter%d', output_filename, user_opts.support_constraint, user_opts.max_iterations), 'im_maxgirf_gpu', 'header_maxgirf', 'r_dcs_maxgirf', 'output_maxgirf', 'user_opts', '-v7.3');
 
-if 0
+if 1
 %% Perform NUFFT reconstruction (single-GPU)
 [im_nufft_gpu, header_nufft, r_dcs_nufft] = siemens_gridding_recon_gpu(ismrmrd_noise_path, ismrmrd_data_path, siemens_dat_path, user_opts);
 save(sprintf('%s_nufft_gpu', output_filename), 'im_nufft_gpu', 'header_nufft', 'r_dcs_nufft', 'user_opts', '-v7.3');
@@ -79,7 +79,9 @@ save(sprintf('%s_king', output_filename), 'im_king', 'header_king', 'r_dcs_king'
 %% Perform SENSE reconstruction (single-GPU)
 [im_sense_gpu, header_sense, r_dcs_sense, output_sense] = siemens_sense_recon_gpu(ismrmrd_noise_path, ismrmrd_data_path, siemens_dat_path, user_opts);
 save(sprintf('%s_sense_gpu', output_filename), 'im_sense_gpu', 'header_sense', 'r_dcs_sense', 'output_sense', 'user_opts', '-v7.3');
+end
 
+if 0
 %% Perform CP-based MaxGIRF reconstruction (single-GPU)
 [im_cpr_gpu, header_cpr, r_dcs_cpr, output_cpr] = siemens_maxgirf_cp_recon_gpu(ismrmrd_noise_path, ismrmrd_data_path, siemens_dat_path, B0map_nlinv, user_opts);
 save(sprintf('%s_cpr_gpu', output_filename), 'im_cpr_gpu', 'header_cpr', 'r_dcs_cpr', 'output_cpr', 'user_opts', '-v7.3');
